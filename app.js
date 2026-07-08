9e712a8a51583b4121f71a001b6bf94cfb05c02d
Chuchineta <lachuchineta@gmail.com>
2026-07-08 15:00:35 +0000

    correcciones

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+// ==============================
+// INVITACIÓN DANIEL & BELÉN
+// ==============================
+
 const envelope = document.getElementById("envelope");
 
-envelope.addEventListener("click",()=>{
+let opened = false;
+let animating = false;
 
-envelope.classList.toggle("open");
+envelope.addEventListener("click", () => {
+
+    if (animating) return;
+
+    animating = true;
+
+    if (!opened) {
+
+        envelope.classList.add("open");
+        opened = true;
+
+    } else {
+
+        envelope.classList.remove("open");
+        opened = false;
+
+    }
+
+    setTimeout(() => {
+
+        animating = false;
+
+    }, 1600);
 
 });
